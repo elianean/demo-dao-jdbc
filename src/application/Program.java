436b2a817233b3ea395d3e2ff61c3fe11de3d4a6
Eliane Akemi <elianean@gmail.com>
2020-04-25 01:51:27 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -29,5 +31,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		DB.closeConnection();
 	}
 }
